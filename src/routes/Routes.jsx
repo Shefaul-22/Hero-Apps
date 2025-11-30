bf92d462e357791d34cbae081e2da212176665ad
Shefaul-22 <cse1805022brur@gmail.com>
2025-11-30 22:27:37 +0600

    Navbar , routes setup added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,8 @@
 import { createBrowserRouter } from "react-router";
 import Home from "../components/Home/Home";
 import RootLayout from "../RootLayout/RootLayout";
+import Apps from "../components/Apps/Apps";
+import Installation from "../components/Installation/Installation";
 
 
 const router = createBrowserRouter([
@@ -14,7 +16,14 @@ const router = createBrowserRouter([
                 index: true,
                 Component: Home
             },
-            
+            {
+                path: "apps",
+                Component: Apps
+            },
+            {
+                path: "installation",
+                Component: Installation
+            }
         ]
 
     },
